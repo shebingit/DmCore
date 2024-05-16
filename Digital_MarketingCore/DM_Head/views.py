2c3ee21d155e85b6481590e0a1eb418951f11b4a
Shebin Shaji <shebinshaji99@gmail.com>
2024-05-16 19:47:43 +0530

    commit- headmodule

--- Digital_MarketingCore/DM_Head/views.py
+++ Digital_MarketingCore/DM_Head/views.py
@@ -20,8 +20,7 @@ import pandas as pd
 from openpyxl.workbook import Workbook
 
 from django.http import HttpResponse
-from django.template.loader import get_template
-from xhtml2pdf import pisa
+
 from django.contrib import messages
 
 #---------new-------
